f3fbe39c81685e9a38fbc068f213f3e035edc404
juan quintero <juanestquintero@gmail.com>
2024-02-06 22:04:27 -0500

    refactor: lambda commands for zip file

--- lambda.sh
+++ lambda.sh
@@ -2,12 +2,10 @@ pip install -t lib -r requirements.txt
 
 (cd lib; zip ../lambda_function.zip -r .)
 
-zip lambda_function.zip -u .env
+mv lambda_function.zip api/ && cd api
 
-mv lambda_function.zip api/
+zip lambda_function.zip -u ./*.py
 
-cd api
-
-zip lambda_function.zip -u ./*
+mv lambda_function.zip ../
 
 cd ..
