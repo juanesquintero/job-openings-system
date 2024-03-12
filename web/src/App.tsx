de4cd5220fdc9e1cf49052f6f0308258db8a0b4b
Juan Quintero <juanestquintero@gmail.com>
2024-03-12 12:23:07 -0500

    style(web): fix app container aligment

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import Chat from "./components/Chat";
 function App() {
   return (
     <>
-      <div>
+      <div className="flex items-center justify-center w-full">
         <a href="https://vitejs.dev" target="_blank">
           <img src={chatLogo} className="logo" alt="Vite logo" />
         </a>
@@ -14,7 +14,7 @@ function App() {
           <img src={cvLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SGC Chat</h1>
+      <h1 className="text-5xl">SGC Chat</h1>
       <div className="card">
         <Chat></Chat>
       </div>
