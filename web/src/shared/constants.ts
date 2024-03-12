1d270ccea3a2aa9d2bed684a534650e68dca222d
Juan Quintero <juanestquintero@gmail.com>
2024-03-12 11:38:27 -0500

    chore(web): user and bot styles

--- web/src/shared/constants.ts
+++ web/src/shared/constants.ts
@@ -1,9 +1,9 @@
 export const MsgStyles = {
-    user: "rounded-e-full rounded-es-full ml-5  pl-4 pr-2 text-left bg-blue-500",
-    bot: "rounded-e-full rounded-es-full mr-5 pr-4 pl-2 text-right bg-green-500",
+    bot: "rounded-e-full rounded-es-full ml-5  pl-4 pr-2 text-left bg-blue-500",
+    user: "rounded-s-full rounded-ee-full mr-5 pr-4 pl-2 text-right bg-green-500 ml-auto",
 }
 
 export const UserStyles = {
-    user: "flex flex-row",
-    bot: "flex flex-row-reverse",
+    bot: "flex flex-row",
+    user: "flex flex-row-reverse space-x-reverse",
 }
