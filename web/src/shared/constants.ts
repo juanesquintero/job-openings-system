cd802c791aa36107214e486870f807d997adc0cb
Juan Quintero <juanestquintero@gmail.com>
2024-03-13 11:11:08 -0500

    chore(web): MsgBoxStyles bot and user

--- web/src/shared/constants.ts
+++ web/src/shared/constants.ts
@@ -1,6 +1,11 @@
+export const MsgBoxStyles = {
+    bot: "ml-5 pl-4 pr-2",
+    user: "mr-5 pr-4 pl-2",
+}
+
 export const MsgStyles = {
-    bot: "rounded-e-full rounded-es-full ml-5  pl-4 pr-2 text-left bg-blue-500",
-    user: "rounded-s-full rounded-ee-full mr-5 pr-4 pl-2 text-right bg-green-500 ml-auto",
+    bot: "rounded-e-full rounded-es-full text-left bg-blue-500",
+    user: "rounded-s-full rounded-ee-full text-right bg-green-500 ml-auto",
 }
 
 export const UserStyles = {
