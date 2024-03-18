0f338be5eb80211fb25a5b6952f44cd8d7adf355
Juan Quintero <juanestquintero@gmail.com>
2024-03-18 17:17:34 -0500

    chore(web): config langs exports

--- web/src/config/i18n/index.ts
+++ web/src/config/i18n/index.ts
@@ -1,8 +1,5 @@
 import i18n from 'i18next';
-import en from './langs/en';
-import es from './langs/es';
-import pt from './langs/pt';
-
+import { en, es, pt } from './langs'
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
