db90fdddc3f91a7956174ea785e4d2241eb6e300
Juan Quintero <juanestquintero@gmail.com>
2024-03-22 09:35:51 -0500

    chore(web): http post body request

--- web/src/shared/types.ts
+++ web/src/shared/types.ts
@@ -6,3 +6,8 @@ export interface IMessage {
 }
 
 export type Sender = "user" | "bot";
+
+export interface IPost {
+    endpoint: string;
+    body: Record<string, string | number | boolean | Date | null | undefined> | unknown;
+}
