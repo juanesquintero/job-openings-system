512bfca76f4793fed43a6f415769269f5b0ca1e3
Juan Quintero <juanestquintero@gmail.com>
2024-03-22 12:41:05 -0500

    chore(web): fetch data util

--- web/src/shared/utils.ts
+++ web/src/shared/utils.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import env from "../config/enviroment";
-import { IPost } from "./types";
+import { IGet, IPost } from "./types";
 
 export const postData = async ({ endpoint, body }: IPost) => {
     try {
@@ -16,6 +16,23 @@ export const postData = async ({ endpoint, body }: IPost) => {
         return data;
     } catch (err) {
         console.error('HTTP Post Error', err);
+        return err;
+    }
+}
+
+export const fecthData = async ({ endpoint }: IGet ) => {
+    try {
+        const { data } = await axios.get(
+            env.apiBaseURL + endpoint,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+        return data;
+    } catch (err) {
+        console.error('HTTP GET Error', err);
         return err;
     }
 }
